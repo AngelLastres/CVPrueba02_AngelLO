4cfea021ed34eb20043dc2e213a963d9a2281a44
Angel Lastres Ortiz <alojalo2007@outlook.com>
2026-02-11 08:53:03 +0100

    Preparación de ficheros para el proyecto

---
+++ CVPrueba02_AngelLO/Miclase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVPrueba02_AngelLO
+{
+    public class Miclase
+    {
+    }
+}
