1f0310b3b41078196d6c3a4661cb547d469e68f2
Alumno <Alumno@DAM1-PC7>
2026-02-11 08:31:58 +0100

    Agregar archivos de proyecto.

---
+++ CVPrueba02_AngelLO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CVPrueba02_AngelLO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
